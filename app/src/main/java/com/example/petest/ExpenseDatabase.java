2f7de0cfa8e849e9a278a32d896ae48f00e5dd32
Vu Quynh Chi <chivqhe176029@fpt.edu.vn>
2024-10-30 00:41:38 +0700

    detail UI

---
+++ app/src/main/java/com/example/petest/ExpenseDatabase.java
@@ -0,0 +1,25 @@
+package com.example.petest;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Expense.class}, version = 1)
+public abstract class ExpenseDatabase extends RoomDatabase {
+
+    private static ExpenseDatabase instance;
+
+    public abstract ExpenseDao expenseDao();
+
+    public static synchronized ExpenseDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                            ExpenseDatabase.class, "expense_database")
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return instance;
+    }
+}
